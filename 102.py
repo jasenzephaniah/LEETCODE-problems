40a1601fad26b75a62669d05c04e7e0974112325
Jasen Zephaniah W <jasenzephaniah11@gmail.com>
2026-06-16 19:37:11 +0530

    Create 102.py

---
+++ 102.py
@@ -0,0 +1,17 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        queue = [root]
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
