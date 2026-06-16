8e246ce9c4a6bd4d95f76fc57d53606fa10b37e2
Jasen Zephaniah W <jasenzephaniah11@gmail.com>
2026-06-16 14:42:52 +0530

    Create 117.py

---
+++ 117.py
@@ -0,0 +1,18 @@
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        queue = [root]
+        while queue:
+            level = []
+            prev = None
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                if prev:
+                    prev.next = node
+                prev = node
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
