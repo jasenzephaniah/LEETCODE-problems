4077a055d73175b6053cb69719209c692c0a2940
Jasen Zephaniah W <jasenzephaniah11@gmail.com>
2026-09-20 13:23:04 +0530

    Create 343.py

---
+++ 343.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def integerBreak(self, n):
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], j * (i - j),j * dp[i - j])
+        return dp[n]
