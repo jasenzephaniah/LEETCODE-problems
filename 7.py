c09a623aa38f5bb2c0dbb9fa4ec35b8bae7025c6
Jasen Zephaniah W <jasenzephaniah11@gmail.com>
2026-09-20 12:43:47 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverse(self, x):
+        s = -1 if x < 0 else 1
+        x = abs(x)
+        rev = 0
+        while x > 0:
+            d = x % 10
+            rev = rev * 10 + d
+            x //= 10
+        rev *= s
+        if rev < -2 ** 31 or rev > 2 ** 31 - 1:
+            return 0
+        return rev
