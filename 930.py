f126c0ea3bb129e43f44cf9290e9e0b00454842b
Jasen Zephaniah W <jasenzephaniah11@gmail.com>
2026-09-23 19:56:13 +0530

    Create 930.py

---
+++ 930.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numSubarraysWithSum(self, nums, goal):
+        def atMost(k):
+            if k < 0:
+                return 0
+            left = 0
+            s = 0
+            ans = 0
+            for right in range(len(nums)):
+                s += nums[right]
+                while s > k:
+                    s -= nums[left]
+                    left += 1
+                ans += right - left + 1
+            return ans
+        return atMost(goal) - atMost(goal - 1)
