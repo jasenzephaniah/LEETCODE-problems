66bb876b0645e25cd9c77cb8bfd3730d683506df
Jasen Zephaniah W <jasenzephaniah11@gmail.com>
2026-09-23 19:17:00 +0530

    Create 74.py

---
+++ 74.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        lo, hi = 0, m * n - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            val = matrix[mid // n][mid % n]
+            if val == target:
+                return True
+            elif val < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return False
